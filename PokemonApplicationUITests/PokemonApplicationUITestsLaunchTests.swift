df6950ac5e57022006d93eb5c331e745c9c6c44f
YuliaSimanavets <mrs.simonovets@gmail.com>
2023-04-11 20:59:14 +0200

    Initial Commit

---
+++ PokemonApplicationUITests/PokemonApplicationUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  PokemonApplicationUITestsLaunchTests.swift
+//  PokemonApplicationUITests
+//
+//  Created by Yuliya on 11/04/2023.
+//
+
+import XCTest
+
+final class PokemonApplicationUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
