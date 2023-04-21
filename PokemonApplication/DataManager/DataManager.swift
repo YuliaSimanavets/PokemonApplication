8b382c278443129bcf48f7235cca215417ebd4e8
YuliaSimanavets <mrs.simonovets@gmail.com>
2023-04-21 11:55:34 +0200

    fixed hardcoded url

--- PokemonApplication/DataManager/DataManager.swift
+++ PokemonApplication/DataManager/DataManager.swift
@@ -16,12 +16,28 @@ protocol DataManagerProtocol {
 
 class DataManager: DataManagerProtocol {
     
+    enum QueryItems: String{
+        case offset
+        case limit
+    }
+    
+    var urlComponents: URLComponents = {
+        var components = URLComponents()
+        components.scheme = "https"
+        components.host = "pokeapi.co"
+        components.path = "/api/v2/pokemon"
+        return components
+    }()
+
     func getPokemons(limit: Int, offset: Int, completion: @escaping (Result<[Pokemon]?, Error>) -> Void) {
         
-        let urlString = "https://pokeapi.co/api/v2/pokemon" + "?" + "offset=\(offset)" + "&" + "limit=\(limit)"
+        let queryItemOffset = URLQueryItem(name: QueryItems.offset.rawValue, value: String(offset))
+        let queryItemLimit = URLQueryItem(name: QueryItems.limit.rawValue, value: String(limit))
+        urlComponents.queryItems = [queryItemOffset, queryItemLimit]
         
-        guard let url = URL(string: urlString) else { return }
-        URLSession.shared.dataTask(with: url) { data, _, error in
+        guard let url = urlComponents.url?.absoluteString else { return }
+        var request = URLRequest(url: URL(string: url)!, timeoutInterval: Double.infinity)
+        URLSession.shared.dataTask(with: request) { data, _, error in
             if let error = error {
                 completion(.failure(error))
                 return
